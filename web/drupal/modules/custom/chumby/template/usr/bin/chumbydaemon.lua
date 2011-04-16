42c7e1ba28edf9407e0ccfcb68571a10ffe4579d
paalsteek <stephan.platz@googlemail.com>
2011-04-16 16:28:44 +0200

    [chumby] dont try to execute flukso-getvalues if there is no ip

--- web/drupal/modules/custom/chumby/template/usr/bin/chumbydaemon.lua
+++ web/drupal/modules/custom/chumby/template/usr/bin/chumbydaemon.lua
@@ -76,31 +76,46 @@ function updateCommand()
 	log("Using " .. ip)
 
 	command = {}
-	command['last_reading'] = {
-					cmd = config.BINPATH .. config.CMD .. 
-								" -l " .. ip .. 
-								" -s " .. config.SENSOR ..
-								" -n " .. config.DATADIR .. "/last_reading" ..
-								" -f chumby-current -o file",
-					interval = 2
-				}
-	command['last_minute'] = {
-					cmd = config.BINPATH .. config.CMD .. 
-								" -l " .. ip .. 
-								" -s " .. config.SENSOR ..
-								" -n " .. config.DATADIR .. "/last_minute" ..
-								" -f chumby-lastminute -o file",
-					interval = 10
-				}
-	command['last_hour'] = {
-					cmd = config.BINPATH .. config.CMD .. 
-								" -l " .. ip .. 
-								" -s " .. config.SENSOR ..
-								" -t " .. config.TOKEN ..
-								" -n " .. config.DATADIR .. "/last_hour" ..
-								" -f chumby-lasthour -o file -i hour",
-					interval = 11
-				}
+	if ip ~= "" then
+		command['last_reading'] = {
+						cmd = config.BINPATH .. config.CMD .. 
+									" -l " .. ip .. 
+									" -s " .. config.SENSOR ..
+									" -n " .. config.DATADIR .. "/last_reading" ..
+									" -f chumby-current -o file",
+						interval = 2
+					}
+		command['last_minute'] = {
+						cmd = config.BINPATH .. config.CMD .. 
+									" -l " .. ip .. 
+									" -s " .. config.SENSOR ..
+									" -n " .. config.DATADIR .. "/last_minute" ..
+									" -f chumby-lastminute -o file",
+						interval = 10
+					}
+		command['last_hour'] = {
+						cmd = config.BINPATH .. config.CMD .. 
+									" -l " .. ip .. 
+									" -s " .. config.SENSOR ..
+									" -t " .. config.TOKEN ..
+									" -n " .. config.DATADIR .. "/last_hour" ..
+									" -f chumby-lasthour -o file -i hour",
+						interval = 11
+					}
+	else
+		command['last_reading'] = {
+						cmd = "",
+						interval = 2
+					}
+		command['last_minute'] = {
+						cmd = "",
+						interval = 10
+					}
+		command['last_hour'] = {
+						cmd = "",
+						interval = 11
+					}
+	end
 end
 
 updateCommand();
