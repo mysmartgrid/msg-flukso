d2c83b340fa462762564fbdfbcce4773d134a25c
ely.oliveira <ely.oliveira@itwm.fraunhofer.de>
2011-05-25 10:29:18 +0200

    The icon 'Join mySmartGrid' is now shown only when the user is not authenticated.

--- web/drupal/themes/msgmobile/page.tpl.php
+++ web/drupal/themes/msgmobile/page.tpl.php
@@ -2,15 +2,11 @@
 <HTML xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>">
   <HEAD><META http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <META http-equiv="X-UA-Compatible" content="IE=EmulateIE7">
-
     <TITLE><?php print $head_title." - ".$title.$node->title ?></TITLE>
     <META http-equiv="content-language" content="<?php print $language->language ?>">
     <META http-equiv="imagetoolbar" content="no">
     <META name="robots" content="index, follow">
     <?php print $styles ?>
-    <!--[if lte IE 6]>
-      <link href="/<?php print path_to_theme(); ?>/style/fraunhofer-ie6.css" rel="stylesheet" type="text/css" media="screen, projection, print" />
-    <![endif]-->
     <SCRIPT type="text/javascript" src="/<?php print path_to_theme(); ?>/style/flash-replace-1.01.js"></SCRIPT>
     <?php print $scripts ?>
     <?php print $head ?>
@@ -20,7 +16,6 @@
 
     <DIV id="page">
 
-      
       <!-- HEADER include start -->
       <DIV id="header" role="banner">
 
@@ -38,26 +33,18 @@
             <?php } ?>
             <?php 
             if ($user->uid) { ?>
-              <A href="/logout">Abmelden</A>
+              <A href="/logout">Abmelden</A>&nbsp;
             <?php } ?>
           </H1>
         </DIV>
       </DIV><!-- #header -->
       <!-- HEADER include end -->
 
-
       <!-- Main content section -->
       <DIV id="doc">
-
-        <?php
-          if (!$user->uid && $_SERVER['REQUEST_URI'] == '/') { ?>
-
-          <!-- Login Form -->
-          <?php
-            if ($left) {?>
-              <DIV id="contentboxes"><?php print $left ?></DIV>
-          <?php } ?>
-
+        <!-- Login Form -->
+        <?php if (!$user->uid && $_SERVER['REQUEST_URI'] == '/' && $left) {?>
+            <DIV id="contentboxes"><?php print $left ?></DIV>
         <?php } ?>
 
         <!-- Text content -->
@@ -73,9 +60,7 @@
           <?php } ?>
 
           <DIV class="section" role="section">
-
             <?php if ($tabs != ""): ?><?php print $tabs ?><?php endif; ?>
-
             <?php if ($help != ""): ?><p id="help"><?php print $help ?></p><?php endif; ?>
             <?php if ($messages != ""): ?><DIV id="message"><?php print $messages ?></DIV><?php endif; ?>
 
@@ -83,9 +68,7 @@
             <?php print($content) ?>
             <!-- end main content -->
           </DIV>
-
         </DIV><!-- #main -->
-
       </DIV><!-- #doc -->
 
       <!-- FOOTER include start -->
@@ -100,7 +83,6 @@
             <li><a href="/content/datenschutzerklärung" >Datenschutzerklärung</a></li>
           </ul>
 
-
           <?php print $footer_message;?>
 
           <!-- Hidden input element to force an update of the screenreader buffer via JavaScript -->
@@ -114,11 +96,9 @@
 
       <!-- Print footer -->
       <DIV id="print-footer">
-        <P>
-          <STRONG>Quelle: Fraunhofer-Gesellschaft – T2 Übersichts-/Indexseite - Forschungsthemen</STRONG><BR>
-          Online im Internet; URL http://ve-166.bi.server.de/international/seitentypen/t2uebersichtseite1<BR>
-          [Stand: 26.02.2011, 08:39 Uhr]<BR>
-        </P>
+        <P><STRONG>Quelle: Fraunhofer-Gesellschaft – T2 Übersichts-/Indexseite - Forschungsthemen</STRONG><BR>
+        Online im Internet; URL http://ve-166.bi.server.de/international/seitentypen/t2uebersichtseite1<BR>
+        [Stand: 26.02.2011, 08:39 Uhr]<BR></P>
       </DIV><!-- #print-footer -->
       <!-- FOOTER include end -->
       
