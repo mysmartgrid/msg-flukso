c72f75c1c72ff765c288a4be8b33980e53805ac5
ely.oliveira <ely.oliveira@itwm.fraunhofer.de>
2012-02-03 10:34:28 +0100

    The chumby image download form now shows widgets' screenshots.

--- web/drupal/modules/custom/chumby/widget.php
+++ web/drupal/modules/custom/chumby/widget.php
@@ -1,10 +1,10 @@
 <?php
   $widget = "
     <widget_instance id=\"$widget->id\">
-      <name>$widget->name</name>
+      <name>" . t($widget->name) . "</name>
       <widget id=\"$widget->id\">
-        <name>$widget->name</name>
-        <description>$widget->description</description>
+        <name>" . t($widget->name) . "</name>
+        <description>" . t($widget->description) . "</description>
         <version>1.0</version>
         <mode mode=\"default\" time=\"30\"/>
         <access sendable=\"false\" deleteable=\"false\" access=\"private\" virtualable=\"false\"/>
