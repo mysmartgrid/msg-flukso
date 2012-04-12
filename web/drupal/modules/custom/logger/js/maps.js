0c84c1f9cc43827f772e8d4deed3757de13372ed
ely.oliveira <ely.oliveira@itwm.fraunhofer.de>
2012-04-12 18:05:54 +0200

    The theme for map fields has been improved.

--- web/drupal/modules/custom/logger/js/maps.js
+++ web/drupal/modules/custom/logger/js/maps.js
@@ -1,5 +1,6 @@
 /**
  * Javascript functions used by module logger to build maps.
+ * It relies on OpenLayers (http://dev.openlayers.org).
  *
  * Copyright (c) 2010 Fraunhofer Institut ITWM (www.itwm.fraunhofer.de)
  *
@@ -18,70 +19,83 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-var map, markers, icon, marker;
+var map;
 
 function createMap(latitude, longitude) {
 
-  map = new OpenLayers.Map('map');
+  var coordinates = new OpenLayers.LonLat(longitude, latitude);
+  var click = createClickControl();
 
-  var basicLayer = new OpenLayers.Layer.WMS( "OpenLayers WMS", "http://vmap0.tiles.osgeo.org/wms/vmap0?", {layers: 'basic'} );
-  map.addLayers([basicLayer]);
+  var markers = new OpenLayers.Layer.Markers("Markers");
+  markers.addMarker(createMarker(coordinates));
 
-  markers = new OpenLayers.Layer.Markers( "Markers" );
-  map.addLayer(markers);
+  map = new OpenLayers.Map('map', {
+
+    layers: [
+      new OpenLayers.Layer.OSM(),
+      markers
+    ],
+
+    controls: [
+      new OpenLayers.Control.Navigation({'zoomWheelEnabled': true}),
+      new OpenLayers.Control.PanZoomBar(),
+      click
+    ],
+
+    zoom: 15,
+    center: coordinates.transform(
+      new OpenLayers.Projection("EPSG:4326"),
+      new OpenLayers.Projection("EPSG:900913")
+    )
+  });
+
+  click.activate();
+}
+
+function createMarker(coordinates) {
 
   var iconSize = new OpenLayers.Size(21, 25);
   var iconOffset = function(size) {
     return new OpenLayers.Pixel(-(iconSize.w/2), -iconSize.h);
   };
-  icon = new OpenLayers.Icon('/sites/all/modules/logger/js/openlayers/img/marker-green.png', iconSize, null, iconOffset);
+  var icon = new OpenLayers.Icon('/sites/all/modules/logger/js/openlayers/img/marker-green.png', iconSize, null, iconOffset);
 
-  marker = new OpenLayers.Marker(new OpenLayers.LonLat(longitude, latitude), icon);
-  markers.addMarker(marker);
-
-  var click = createClickControl();
-  map.addControl(click);
-  map.addControl(new OpenLayers.Control.Navigation({'zoomWheelEnabled': true}));
-
-  map.zoomToExtent(new OpenLayers.Bounds(9.0, 47.0, 14.0, 57.0));
-
-  click.activate();
+  return new OpenLayers.Marker(coordinates, icon);
 }
 
 function createClickControl() {
+
   OpenLayers.Control.Click = OpenLayers.Class(OpenLayers.Control, {
-    defaultHandlerOptions: {
-      'single': true,
-      'double': false,
-      'pixelTolerance': 0,
-      'stopSingle': false,
-      'stopDouble': false
-    },
 
     initialize: function(options) {
-      this.handlerOptions = OpenLayers.Util.extend(
-        {}, this.defaultHandlerOptions
-      );
+      this.handlerOptions = OpenLayers.Util.extend({}, this.defaultHandlerOptions);
       OpenLayers.Control.prototype.initialize.apply(this, arguments);
       this.handler = new OpenLayers.Handler.Click(
-        this, {
-          'click': this.trigger
-        }, this.handlerOptions
+        this, {'click': this.trigger}
       );
     },
 
     trigger: function(e) {
+
       var coordinates = map.getLonLatFromViewPortPx(e.xy);
-      var longitude = document.getElementById('edit-coordinates_longitude');
-      var latitude = document.getElementById('edit-coordinates_latitude');
 
-      longitude.value = coordinates.lon.toFixed(6);
-      latitude.value = coordinates.lat.toFixed(6);
+      var markers = map.getLayersByName('Markers')[0];
+      markers.clearMarkers();
+      markers.addMarker(createMarker(coordinates));
 
-      markers.removeMarker(marker);
-      marker = new OpenLayers.Marker(new OpenLayers.LonLat(longitude.value, latitude.value), icon);
-      markers.addMarker(marker);
+      coordinates.transform(map.getProjectionObject(), new OpenLayers.Projection("EPSG:4326"));
+      updateFormFields(coordinates);
     }
   });
+
   return new OpenLayers.Control.Click();
+}
+
+function updateFormFields(coordinates) {
+
+  var longitudeField = document.getElementById('edit-coordinates_longitude');
+  var latitudeField = document.getElementById('edit-coordinates_latitude');
+
+  longitudeField.value = coordinates.lon.toFixed(8);
+  latitudeField.value = coordinates.lat.toFixed(8);
 }
