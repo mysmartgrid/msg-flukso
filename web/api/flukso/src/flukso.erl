c8cadd9bb54ae0f3b977f7bf8b5b6e539c6c57ed
ely.oliveira <ely.oliveira@itwm.fraunhofer.de>
2012-05-15 17:07:00 +0200

    The SQL statement for sensor insertion now contains default values for latitude, longitude, and price.

--- web/api/flukso/src/flukso.erl
+++ web/api/flukso/src/flukso.erl
@@ -48,7 +48,7 @@ mysql_prepare() ->
     mysql:prepare(device_update, <<"UPDATE logger_devices SET access = ?, version = ?, upgrade = ?, resets = ?, uptime = ?, memtotal = ?, memfree = ?, memcached = ?, membuffers = ?, sha = ? WHERE device = ?">>),
     mysql:prepare(event_insert, <<"INSERT INTO event_log (device, event_id, time) VALUES (?, ?, ?)">>),
     mysql:prepare(device_insert, <<"INSERT INTO logger_devices (device, serial, uid, sha, created, access, version, upgrade, resets, uptime, memtotal, memfree, memcached, membuffers, uart_oe, sensor, country) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)">>),
-    mysql:prepare(sensor_insert, <<"INSERT INTO logger_meters (meter, uid, device, created, access, type, function, phase, constant, value, factor, unit) SELECT ?, uid, device, ?, ?, ?, ?, ?, ?, ?, ?, ? FROM logger_devices WHERE device = ?">>),
+    mysql:prepare(sensor_insert, <<"INSERT INTO logger_meters (meter, uid, device, created, access, type, function, phase, constant, value, factor, unit, price, latitude, longitude) SELECT ?, uid, device, ?, ?, ?, ?, ?, ?, ?, ?, ?, 0.18, 49.444710, 7.769031 FROM logger_devices WHERE device = ?">>),
     mysql:prepare(token_insert, <<"INSERT INTO logger_tokens (token, meter, permissions) VALUES (?, ?, ?)">>),
     mysql:prepare(support_slot, <<"SELECT username, host, port FROM device_support_slot WHERE device = ?">>).
 
