1bd3501c9840892a8571aee9f93b5d601ee52c61
ely.oliveira <ely.oliveira@itwm.fraunhofer.de>
2013-04-05 18:45:25 +0200

    Javascript code has been moved to the bottom of the page.

--- web/drupal/themes/msgweb/html.tpl.php
+++ web/drupal/themes/msgweb/html.tpl.php
@@ -47,7 +47,6 @@
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
-  <?php print $scripts; ?>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <div id="skip-link">
@@ -55,6 +54,7 @@
   </div>
   <?php print $page_top; ?>
   <?php print $page; ?>
+  <?php print $scripts; ?>
   <?php print $page_bottom; ?>
 </body>
 </html>
