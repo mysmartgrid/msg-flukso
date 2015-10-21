a2fac90e6bc6c623a0cb13821a6b1639524b0b97
Kai Krueger <kai.krueger@itwm.fraunhofer.de>
2015-10-21 13:06:58 +0000

    ipvalidation, review config

--- web/drupal/modules/custom/logger/js/amperix.js
+++ web/drupal/modules/custom/logger/js/amperix.js
@@ -38,6 +38,9 @@ jQuery.validator.addMethod("integer", function(value, element) {
 jQuery.validator.addMethod("string", function(value, element) {
 			       return this.optional(element) || /^([a-zA-Z_0-9])+$/.test(value);
 			   }, "Ungueltiges Zeichen: erlaubt: a-zA-Z0-9_");
+jQuery.validator.addMethod("ipaddress", function(value, element) {
+			       return this.optional(element) || /^([0-9])+.([0-9])+.([0-9])+.([0-9])+$/.test(value);
+			   }, "Ungueltiges Zeichen: erlaubt: 0-9.");
 jQuery.validator.addMethod("require_from_group", function(value, element, params) {
 			       console.log("p0:" + params[0]);
 			       console.log("p1:" + params[1]);
@@ -60,17 +63,19 @@ function toggleNetwork(field1, field2, field3, field4, state) {
   var thisid = field1.attr('id');
 
   if(state) {
-    field1.prop('hidden', false).change()
-    field1.prop('required', true).change()
+    field1.prop('hidden', false).change();
+    field1.prop('required', true).change();
 
-    field2.prop('hidden', true).change()
-    field2.prop('required', false).change()
+    field2.prop('hidden', true).change();
+    field2.prop('required', false).change();
 
-    field3.prop('checked', false).change()
-  } else {
-    field1.prop('hidden', true).change()
-    field1.prop('required', true).change()
-    field2.prop('required', true).change()
+    field3.prop('checked', false).change();
+	} else {
+    field1.prop('hidden', true).change();
+    field1.prop('required', true).change();
+    field2.prop('required', true).change();
+		
+    field3.prop('checked', true).change();
   }
   console.log("my id is: "+thisid);
   if( thisid == 'nw-wlan') {
@@ -102,19 +107,19 @@ function toggleHidden(field1, field2, state) {
   if(state) {
     field1.prop('hidden', false).change();
     nwip = $('#'+field2+'-ip');
-    nwip.rules("add", { required: true, string: true, messages: { required: "Bitte SSID eingeben.",} } );
+    nwip.rules("add", { required: true, ipaddress: true, messages: { required: "Bitte SSID eingeben.",} } );
     $('#'+field2+'-ip').closest('.form-group').removeClass('has-success').addClass('has-error');
     $('#'+field2+'-ip1').removeClass('glyphicon-ok').addClass('glyphicon-remove');  
 
-    $('#'+field2+'-gw').rules("add", { required: true, string: true, messages: { required: "Bitte SSID eingeben.",} } );
+    $('#'+field2+'-gw').rules("add", { required: true, ipaddress: true, messages: { required: "Bitte SSID eingeben.",} } );
     $('#'+field2+'-gw').closest('.form-group').removeClass('has-success').addClass('has-error');
     $('#'+field2+'-gw1').removeClass('glyphicon-ok').addClass('glyphicon-remove');  
 
-    $('#'+field2+'-mask').rules("add", { required: true, string: true, messages: { required: "Bitte SSID eingeben.",} } );
+    $('#'+field2+'-mask').rules("add", { required: true, ipaddress: true, messages: { required: "Bitte SSID eingeben.",} } );
     $('#'+field2+'-mask').closest('.form-group').removeClass('has-success').addClass('has-error');
     $('#'+field2+'-mask1').removeClass('glyphicon-ok').addClass('glyphicon-remove');  
 
-    $('#'+field2+'-ns').rules("add", { required: true, string: true, messages: { required: "Bitte SSID eingeben.",} } );
+    $('#'+field2+'-ns').rules("add", { required: true, ipaddress: true, messages: { required: "Bitte SSID eingeben.",} } );
     $('#'+field2+'-ns').closest('.form-group').removeClass('has-success').addClass('has-error');
     $('#'+field2+'-ns1').removeClass('glyphicon-ok').addClass('glyphicon-remove');  
   } else {
@@ -259,8 +264,6 @@ function createHallSensorHTML(no,y,z) {
   r = r + '        <div class="form-group has-feedback">';
   r = r + '<input type="text" class="form-control" placeholder="Sensorname" aria-describedby="basic-addon2"';
   r = r + ' name="sensorname[' + no + ']" id="sensor' + no + '-name" disabled';
-  //r = r + '     data-validation-regex="/^(?![0-9])((?!admin).)*$/i"';
-  // r = r + '     data-validation-regex-message="Word 'admin' is not allowed in $ ..."';
   r = r + ' data-validation-required-message="Bitte einen g&uuml;ltigen Name angeben." />';
   r = r + '        <span class="glyphicon form-control-feedback" id="sensor' + no + '-name1"></span>';
   r = r + '        </div>';
@@ -296,6 +299,8 @@ function createHallSensor(no,y,z) {
   });
   $('#sensor'+no+'-name').on('click', function() {  $('#logger-deviceconfig2-form').valid(); });
   $('#sensor'+no+'-name').on('change', function() {  $('#logger-deviceconfig2-form').valid(); });
+
+	$('#sensor' + no + 'Form').click(function(event){$('#sensor'+no+'_checkbox').bootstrapSwitch('state', true);toggleSensor($('#sensor'+no+'-name'), $('#sensor'+no+'-extras'),true);});
 }
 
 /*
@@ -369,7 +374,7 @@ function nextPage(cPage, nPage) {
 
   if( form1.valid() ) {
     //var tab = $(this).attr('data-tab-destination');
-    if( cPage == 'configuration' ) {
+    if( nPage == 'synchronisation' ) {
       console.log(' Submit1...: '+cPage);
       //form1.submit();
       console.log(' Submit2...: '+cPage);
@@ -377,6 +382,76 @@ function nextPage(cPage, nPage) {
       console.log(' Submit3...: '+cPage);
     } else {
       console.log(' Next tab');
+			if( nPage == 'configuration' ) {
+				if($('#nw_lan_checkbox').is(":checked")) { 
+					$('#cfg-lan').html("LAN");
+					$('#cfg-wifi').prop("hidden",true).change();
+				} else if($('#nw_wlan_checkbox').is(":checked")) { 
+					$('#cfg-lan').html("WiFi");
+					$('#cfg-wifi').prop("hidden",false).change();
+					$('#cfg-wlan-ssid').html($('#nw-wlan-ssid').val());
+					$('#cfg-wlan-crypto').html($('#nw-wlan-crypto').val());
+					$('#cfg-wlan-key').html($('#nw-wlan-key').val());
+				} else { $('#cfg-lan').html("unknown"); }
+
+				if($('#lan-dhcp').val() == "dhcp") {
+					$('#cfg-protocol').html("DHCP");
+					$('#cfg-static').prop("hidden", true).change();
+				} else {
+					$('#cfg-protocol').html("Static");
+					$('#cfg-lan-ip').html($('#lan-ip').val());
+					$('#cfg-lan-gw').html($('#lan-gw').val());
+					$('#cfg-lan-mask').html($('#lan-mask').val());
+					$('#cfg-lan-ns').html($('#lan-ns').val());
+				}
+			}
+			if($('#sensor1_checkbox').is(':checked')) {
+				$('#cfg-sensor1').removeClass('glyphicon-remove').addClass('glyphicon-ok');  
+				//$('#cfg-sensor1').html("aktiviert");
+				$('#cfg-sensor1-name').html($('#sensor1-name').val());
+				$('#cfg-sensor1-extras').html($('#sensor1-extras').val() + ' A');
+			} else {
+				$('#cfg-sensor1').removeClass('glyphicon-ok').addClass('glyphicon-remove');  
+				$('#cfg-sensor1-name').html('');
+				//$('#cfg-sensor1').html("deaktiviert");
+				$('#cfg-sensor1-extras').html('');
+			}
+			if($('#sensor2_checkbox').is(':checked')) {
+				$('#cfg-sensor2').removeClass('glyphicon-remove').addClass('glyphicon-ok');  
+				$('#cfg-sensor2-name').html($('#sensor2-name').val());
+				$('#cfg-sensor2-extras').html($('#sensor2-extras').val() + ' A');
+			} else {
+				$('#cfg-sensor2').removeClass('glyphicon-ok').addClass('glyphicon-remove');  
+				$('#cfg-sensor2-name').html('');
+				$('#cfg-sensor2-extras').html('');
+			}
+			if($('#sensor3_checkbox').is(':checked')) {
+				$('#cfg-sensor3').removeClass('glyphicon-remove').addClass('glyphicon-ok');  
+				$('#cfg-sensor3-name').html($('#sensor3-name').val());
+				$('#cfg-sensor3-extras').html($('#sensor3-extras').val() + ' A');
+			} else {
+				$('#cfg-sensor3').removeClass('glyphicon-ok').addClass('glyphicon-remove');  
+				$('#cfg-sensor3-name').html('');
+				$('#cfg-sensor3-extras').html('');
+			}
+			if($('#sensor4_checkbox').is(':checked')) {
+				$('#cfg-sensor4').removeClass('glyphicon-remove').addClass('glyphicon-ok');  
+				$('#cfg-sensor4-name').html($('#sensor4-name').val());
+				$('#cfg-sensor4-extras').html($('#sensor4-extras').val() + ' Impuls/kWh');
+			} else {
+				$('#cfg-sensor4').removeClass('glyphicon-ok').addClass('glyphicon-remove');  
+				$('#cfg-sensor4-name').html('');
+				$('#cfg-sensor4-extras').html('');
+			}
+			if($('#sensor5_checkbox').is(':checked')) {
+				$('#cfg-sensor5').removeClass('glyphicon-remove').addClass('glyphicon-ok');  
+				$('#cfg-sensor5-name').html($('#sensor5-name').val());
+				$('#cfg-sensor5-extras').html($('#sensor5-extras').val() + ' Impuls/kWh');
+			} else {
+				$('#cfg-sensor5').removeClass('glyphicon-ok').addClass('glyphicon-remove');
+				$('#cfg-sensor5-name').html('');
+				$('#cfg-sensor5-extras').html('');
+			}
 			switchtab(nPage);
     }
   } else {
@@ -388,8 +463,8 @@ function nextPage(cPage, nPage) {
 				alert( "Es muss mindestens 1 Sensor aktiviert werden." );
       } else {
 				alert( "Die Eingabe ist noch fehlerhaft bzw unvollstaedig." );
-      }
-    }
+			}
+		}
 		return false;
   }
 }
@@ -422,34 +497,29 @@ function fillForm() {
   txt = txt + '<div class="panel-heading"><center>Netzwerkkonfiguration</center></div>';
   txt = txt + '      <div class="panel-body" id="nw" >';
   txt = txt + '        <div class="row">';
-  txt = txt + '          <div class="col-sm-6 form-group">';
-  txt = txt + '            <div class="panel panel-default">';
-  txt = txt + '              <div class="panel-heading">';
-  txt = txt + '                <div class="input-group panel-title">';
-  txt = txt + '                  <h3 class="panel-title">LAN Einstellung</h3>';
+  txt = txt + '          <div class="col-sm-2 form-group">';
+  txt = txt + '                <div class="input-group">';
   txt = txt + '                  <input type="checkbox" id="nw_lan_checkbox" name="nw_enabled[]" value="1" ';
   txt = txt + '                         data-size="mini"';
   txt = txt + '                         class="network-group">';
   txt = txt + '                </div>';
-  txt = txt + '              </div>';
-  txt = txt + '              <div class="panel-body" id="nw-lan" hidden>';
-  //txt = txt + '                <script>createNetworkConfig(\'lan\');</script>';
-  txt = txt + createNetworkConfigHTML('lan');
-  txt = txt + '              </div>';
-  txt = txt + '            </div>';
   txt = txt + '          </div>';
-  txt = txt + '          <div class="col-sm-6">';
-  txt = txt + '            <div class="panel panel-default">';
-  txt = txt + '              <div class="panel-heading">';
-  txt = txt + '                <h3 class="panel-title">WLAN Einstellung</h3>';
+  txt = txt + '          <div class="col-sm-1 form-group">LAN';
+  txt = txt + '          </div>';
+  txt = txt + '          <div class="col-sm-2 form-group"></div>';
+  txt = txt + '          <div class="col-sm-2">';
   txt = txt + '                <div class="input-group">';
   txt = txt + '                  <input type="checkbox" id="nw_wlan_checkbox" name="nw_enabled[]" value="2" ';
   txt = txt + '                         data-size="mini"';
   txt = txt + '                         class="network-group">';
   txt = txt + '                </div>';
-  txt = txt + '              </div>';
+  txt = txt + '          </div>';
+  txt = txt + '          <div class="col-sm-1 form-group">WiFi';
+  txt = txt + '          </div>';
+  txt = txt + '          <div class="col-sm-2 form-group"></div>';
+  txt = txt + '        </div>';
 
-  txt = txt + '              <div class="panel-body" id="nw-wlan" hidden>';
+  txt = txt + '        <div id="nw-wlan" hidden>'; // WiFi-config
   txt = txt + '                <div id="wlanForm" class="row">';
   txt = txt + '                  <div class="col-sm-5">SSID:</div>';
   txt = txt + '                  <div class="col-sm-7">';
@@ -484,12 +554,9 @@ function fillForm() {
   txt = txt + '                  </div>';
   txt = txt + '                  </div>';
   txt = txt + '                </div>';
-  //txt = txt + '                <script>createNetworkConfig(\'wlan\');</script>';
-  txt = txt + createNetworkConfigHTML('wlan');
-  txt = txt + '              </div>';
-  txt = txt + '            </div>';
-  txt = txt + '          </div>';
   txt = txt + '        </div>';
+  txt = txt + createNetworkConfigHTML('lan');
+
   txt = txt + '        <!-- end row network -->';
   txt = txt + '        <div class="row">';
   txt = txt + '          <div class="col-lg-12" align="right">';
@@ -536,44 +603,72 @@ function fillForm() {
   txt = txt + '    <div class="panel-heading"><center>Konfiguration</center></div>';
   txt = txt + '    <div class="panel-body">';
   txt = txt + '      <div class="row">';
-  txt = txt + '        <div class="col-sm-5">Anschluss:</div>';
-  txt = txt + '        <div class="col-sm-6"></div>';
+  txt = txt + '        <div class="col-sm-3">Anschluss:</div>';
+  txt = txt + '        <div class="col-sm-6" id="cfg-lan"></div>';
+  txt = txt + '      </div>';
+  txt = txt + '      <div id="cfg-wifi"><div class="row">';
+  txt = txt + '        <div class="col-sm-3">ESSID:</div>';
+  txt = txt + '        <div class="col-sm-6" id="cfg-wlan-ssid"></div>';
   txt = txt + '      </div>';
   txt = txt + '      <div class="row">';
-  txt = txt + '        <div class="col-sm-5">ESSID:</div>';
-  txt = txt + '        <div class="col-sm-6" id="nw-wlan-ssid"></div>';
+  txt = txt + '        <div class="col-sm-3">Verschl&uuml;sselung:</div>';
+  txt = txt + '        <div class="col-sm-6" id="cfg-wlan-crypto"></div>';
   txt = txt + '      </div>';
   txt = txt + '      <div class="row">';
-  txt = txt + '        <div class="col-sm-5">Verschl&uuml;sselung:</div>';
-  txt = txt + '        <div class="col-sm-6" id="nw-wlan-crypto"></div>';
+  txt = txt + '        <div class="col-sm-3">Passwort/psk:</div>';
+  txt = txt + '        <div class="col-sm-6" id="cfg-wlan-key"></div>';
+  txt = txt + '      </div></div>';
+  txt = txt + '      <div class="row">';
+  txt = txt + '        <div class="col-sm-3">Protocol:</div>';
+  txt = txt + '        <div class="col-sm-6" id="cfg-protocol"></div>';
+  txt = txt + '      </div>';
+
+  txt = txt + '      <div id="cfg-static"><div class="row">';
+  txt = txt + '        <div class="col-sm-3">IP Adresse:</div>';
+  txt = txt + '        <div class="col-sm-6" id="cfg-lan-ip"></div>';
   txt = txt + '      </div>';
   txt = txt + '      <div class="row">';
-  txt = txt + '        <div class="col-sm-5">Passwort/psk:</div>';
-  txt = txt + '        <div class="col-sm-6" id="nw-wlan-key"></div>';
+  txt = txt + '        <div class="col-sm-3">Netzmaske:</div>';
+  txt = txt + '        <div class="col-sm-6" id="cfg-lan-mask"></div>';
   txt = txt + '      </div>';
   txt = txt + '      <div class="row">';
-  txt = txt + '        <div class="col-sm-5">Protocol:</div>';
-  txt = txt + '        <div class="col-sm-6" id="lan-dhcp2"></div>';
+  txt = txt + '        <div class="col-sm-3">Gateway:</div>';
+  txt = txt + '        <div class="col-sm-6" id="cfg-lan-gw"></div>';
   txt = txt + '      </div>';
   txt = txt + '      <div class="row">';
-  txt = txt + '        <div class="col-sm-5">IP Adresse:</div>';
-  txt = txt + '        <div class="col-sm-6"></div>';
+  txt = txt + '        <div class="col-sm-3">Nameserver:</div>';
+  txt = txt + '        <div class="col-sm-6" id="cfg-lan-ns"></div>';
+  txt = txt + '      </div></div>';
+
+  txt = txt + '      <div class="row">';
+  txt = txt + '        <div class="col-sm-3">Sensor 1:</div>';
+  txt = txt + '        <div class="col-sm-1 glyphicon" id="cfg-sensor1"></div>';
+  txt = txt + '        <div class="col-sm-3" id="cfg-sensor1-name"></div>';
+  txt = txt + '        <div class="col-sm-3" id="cfg-sensor1-extras"></div>';
   txt = txt + '      </div>';
   txt = txt + '      <div class="row">';
-  txt = txt + '        <div class="col-sm-5">Netzmaske:</div>';
-  txt = txt + '        <div class="col-sm-6"></div>';
+  txt = txt + '        <div class="col-sm-3">Sensor 2:</div>';
+  txt = txt + '        <div class="col-sm-1 glyphicon" id="cfg-sensor2"></div>';
+  txt = txt + '        <div class="col-sm-3" id="cfg-sensor2-name"></div>';
+  txt = txt + '        <div class="col-sm-3" id="cfg-sensor2-extras">A</div>';
   txt = txt + '      </div>';
   txt = txt + '      <div class="row">';
-  txt = txt + '        <div class="col-sm-5">Gateway:</div>';
-  txt = txt + '        <div class="col-sm-6"></div>';
+  txt = txt + '        <div class="col-sm-3">Sensor 3:</div>';
+  txt = txt + '        <div class="col-sm-1 glyphicon" id="cfg-sensor3"></div>';
+  txt = txt + '        <div class="col-sm-3" id="cfg-sensor3-name"></div>';
+  txt = txt + '        <div class="col-sm-3" id="cfg-sensor3-extras">A</div>';
   txt = txt + '      </div>';
   txt = txt + '      <div class="row">';
-  txt = txt + '        <div class="col-sm-5">Nameserver:</div>';
-  txt = txt + '        <div class="col-sm-6"></div>';
+  txt = txt + '        <div class="col-sm-3">Sensor 4:</div>';
+  txt = txt + '        <div class="col-sm-1 glyphicon" id="cfg-sensor4"></div>';
+  txt = txt + '        <div class="col-sm-3" id="cfg-sensor4-name"></div>';
+  txt = txt + '        <div class="col-sm-3" id="cfg-sensor4-extras">Impuls/kWh</div>';
   txt = txt + '      </div>';
   txt = txt + '      <div class="row">';
-  txt = txt + '        <div class="col-sm-5">Sensor 1:</div>';
-  txt = txt + '        <div class="col-sm-6"></div>';
+  txt = txt + '        <div class="col-sm-3">Sensor 5:</div>';
+  txt = txt + '        <div class="col-sm-1 glyphicon" id="cfg-sensor5"></div>';
+  txt = txt + '        <div class="col-sm-3" id="cfg-sensor5-name"></div>';
+  txt = txt + '        <div class="col-sm-3" id="cfg-sensor5-extras">Impuls/kWh</div>';
   txt = txt + '      </div>';
 
 
@@ -610,7 +705,7 @@ function fillForm() {
   form1.append(html);
 
   createNetworkConfig('lan');
-  createNetworkConfig('wlan');
+  //createNetworkConfig('wlan');
   createHallSensor(1,2,3);
   createHallSensor(2,2,3);
   createHallSensor(3,2,3);
@@ -659,20 +754,20 @@ function fillForm() {
         } 
   });
 
-    $('.network-group').rules("add", { required: true } );
-    $('.sensor-group').rules("add", { required: true } );
-    //$("#logger-deviceconfig2-form").validate().settings.onsubmit = false;
+	$('.network-group').rules("add", { required: true, string: true, messages: { required: ""}, } );
+	$('.sensor-group').rules("add", { required: true, string: false, messages: { required: false}, } );
+	//$("#logger-deviceconfig2-form").validate().settings.onsubmit = false;
 
-    $("#nw_lan_checkbox").bootstrapSwitch('state', false);
+    $("#nw_lan_checkbox").bootstrapSwitch('state', true);
 
     $("#nw_wlan_checkbox").bootstrapSwitch('state', false);
 
     $('#nw_lan_checkbox').on('switchChange.bootstrapSwitch', function(event, state) {
-			       toggleNetwork($('#nw-lan'), $('#nw-wlan'), $('#nw_wlan_checkbox'), $('#nw_lan_checkbox'), state);
-			     });
+															 toggleNetwork($('#nw-lan'), $('#nw-wlan'), $('#nw_wlan_checkbox'), $('#nw_lan_checkbox'), state);
+														 });
     $('#nw_wlan_checkbox').on('switchChange.bootstrapSwitch', function(event, state) {
-				toggleNetwork($('#nw-wlan'), $('#nw-lan'), $('#nw_lan_checkbox'), $('#nw_wlan_checkbox'), state);
-			      });
+																toggleNetwork($('#nw-wlan'), $('#nw-lan'), $('#nw_lan_checkbox'), $('#nw_wlan_checkbox'), state);
+															});
   console.log('Fillform - done');
 
 }
